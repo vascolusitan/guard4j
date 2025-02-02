600d60fe60dc10f5a6ab74a3cd728b5037133165
Vasco Lusitano <vasco.lusitano@five9.com>
2025-02-02 12:18:55 +0000

    Project skeleton

---
+++ src/main/java/com/github/guard4j/Guard4jApplication.java
@@ -0,0 +1,13 @@
+package com.github.guard4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guard4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Guard4jApplication.class, args);
+	}
+
+}
